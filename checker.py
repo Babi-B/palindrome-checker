8040e4ecd86dacdb8b933e8b56d699d23a66838d
Babi-B <babibyoolah@gmail.com>
2023-09-01 22:04:07 +0100

    Can detect whether a text is a palindrome or not

--- checker.py
+++ checker.py
@@ -1,19 +1,39 @@
 from intro import intro
+from clean_palindrome import clean_palindrome
 
 def checker():
+    isPalindrome = True
+    tracker = 0
+
     # Get the palindrome from the intro module
     palindrome = intro()
     target = clean_palindrome(palindrome)
+
+    # Get the turning point
+    mid_indx = round(len(target) / 2)
+    # pivot = target[mid_indx]
+    x = y = mid_indx
+
+
+    while x < len(target)  and y > 0 and isPalindrome:
+        x += 1
+        y -= 1
+        print(f"{target[x]}, {target[y]}")
+        if target[x] == target[y]:
+            print(f'Hello1 {x},{y}')
+            tracker += 1
+            isPalindrome = True
+        else:
+            print(f'Hello2 {x},{y}')
+            isPalindrome = False
+
+    print(f"Your text, {palindrome}, is a palindrome") if (isPalindrome and tracker == mid_indx) else print(f"Your text, {palindrome}, is not a palindrome")
+
+
+
         
     # target = palindrome.replace(" ","")
     print("")
     print(f"This is a {target}")
 
 
-# Remove whitespaces, punctuation, numbers etc
-def clean_palindrome(palindrome):
-    target = ''
-    for pal in palindrome:
-        if pal.isalpha():
-            target += pal
-    return target
